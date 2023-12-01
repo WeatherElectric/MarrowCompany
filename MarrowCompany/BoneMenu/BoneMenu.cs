d8a696aba2c24fd054e820406847f213265c513a
Mae <maya@carrionandon.me>
2023-11-30 19:49:23 -0800

    Add debug menu

--- MarrowCompany/BoneMenu/BoneMenu.cs
+++ MarrowCompany/BoneMenu/BoneMenu.cs
@@ -2,9 +2,27 @@
 
 internal static class BoneMenu
 {
+    private static int _money;
     public static void Setup()
     {
         MenuCategory mainCat = MenuManager.CreateCategory("Weather Electric", "#6FBDFF");
         MenuCategory subCat = mainCat.CreateCategory("Marrow Company", Color.red);
+#if DEBUG
+        MenuCategory debugCat = mainCat.CreateCategory("Debug", Color.green);
+        debugCat.CreateIntElement("Money", Color.white, 1, 1, 1, 100, v => _money = v);
+        debugCat.CreateFunctionElement("Add Money", Color.white, () => SaveManager.IncrementField(JsonField.Money, _money));
+        debugCat.CreateFunctionElement("Show Money", Color.white, () =>
+        {
+            var notification = new Notification()
+            {
+                Title = "Money",
+                Message = $"You have {_money} money!",
+                Type = NotificationType.Information,
+                PopupLength = 2f,
+                ShowTitleOnPopup = true
+            };
+            Notifier.Send(notification);
+        });
+#endif
     }
 }
